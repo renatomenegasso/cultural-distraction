5a479fc3270e1df51b4b61d62db9a9a3d2d30caa
Renato Menegasso <renato.menegasso@bidu.com.br>
2019-01-17 15:17:35 -0200

    Load on time version before redirect

--- assets/js/background.js
+++ assets/js/background.js
@@ -2,9 +2,14 @@
   let currentBlocks = {};
 
   chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-    if(changeInfo.url && currentBlocks['fb'] && changeInfo.url.indexOf('facebook') > -1) {
-      chrome.tabs.update(tab.id, {
-        url: 'https://en.wikipedia.org/wiki/Special:Random'
+
+    if(changeInfo.url && changeInfo.url.indexOf('facebook') > -1) {
+      chrome.storage.sync.get(['fb'], (result) => {
+        if(result['fb']) {
+          chrome.tabs.update(tab.id, {
+            url: 'https://en.wikipedia.org/wiki/Special:Random'
+          });
+        }
       });
     }
   });
